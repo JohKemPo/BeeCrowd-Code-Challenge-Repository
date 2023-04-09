56647ec5b323664824f428b9cc6546a99939d9d6
JohKenPo <joaovitormoraes.cc@gmail.com>
2023-04-09 20:39:46 -0300

    1533

---
+++ 1533.c
@@ -0,0 +1,58 @@
+/**
+ * @file 1533.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-09
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+/*
+ [Problema 1533 do URI Online Judge] John Watson, mesmo após anos trabalhando ao lado
+de Sherlock Holmes, nunca conseguiu entender como ele consegue descobrir quem é o assassino
+com tanta facilidade. Em uma certa noite, porém, Sherlock bebeu mais do que devia e acabou
+contando o segredo a John. “Elementar, meu caro Watson”, disse Sherlock Holmes. “Nunca é o
+mais suspeito, mas sim o segundo mais suspeito”. Após descobrir o segredo, John decidiu
+resolver um crime por conta própria, só para testar se aquilo fazia sentido ou se era apenas
+conversa de bêbado. Dada uma lista com N inteiros, representando o quanto cada pessoa é
+suspeita, ajude John Watson a decidir quem é o assassino, de acordo com o método citado.
+Haverá diversos casos de teste. Cada caso de teste inicia com um inteiro N (2 ≤ N ≤ 1000),
+representando o número de suspeitos. Em seguida haverá N inteiros distintos, onde o i-ésimo
+inteiro, para todo 1 ≤ i ≤ N, representa o quão suspeita a i-ésima pessoa é, de acordo com a
+classificação dada por John Watson. Seja V o valor do i-ésimo inteiro, 1 ≤ V ≤ 10000. O último
+caso de teste é indicado quando N = 0, o qual não deverá ser processado. Para cada caso de
+teste imprima uma linha, contendo um inteiro, representando o indice do assassino, de acordo
+com o método citado.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    while (scanf("%d", &n) == 1 && n != 0) {
+        int suspects[n];
+        int index[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &suspects[i]);
+            index[i] = i + 1;
+        }
+        // Ordena o array de suspeitos e o array de índices ao mesmo tempo
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (suspects[i] < suspects[j]) {
+                    int temp = suspects[i];
+                    suspects[i] = suspects[j];
+                    suspects[j] = temp;
+                    temp = index[i];
+                    index[i] = index[j];
+                    index[j] = temp;
+                }
+            }
+        }
+        printf("%d\n", index[1]); // Imprime o índice do segundo maior suspeito
+    }
+    return 0;
+}
